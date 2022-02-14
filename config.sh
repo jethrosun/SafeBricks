aef777a6b42c3aecbec59c61ca3b786fe68b6648
Shuwen Sun <sun.shuw@northeastern.edu>
2022-02-14 17:01:39 -0500

    dope

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
-
 #!/bin/bash
 
 MODE=release
 # MODE=debug
 # PORT_OPTIONS="dpdk:eth_pcap0,rx_pcap=../traffic/caida18_real.pcap,tx_pcap=/tmp/out.pcap"
-PORT_OPTIONS="0000:01:00.1"
-TASK=monitoring
+# PORT_OPTIONS="0000:01:00.1"
+PORT_OPTIONS="0000:01:00.0,0000:01:00.1"
+TASK=traversal
